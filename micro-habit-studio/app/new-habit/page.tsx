d75f00cdb4d2ca79a16325c9fc588cbea2b2edf3
SamudiHansani <samudihansani@gmail.com>
2026-01-23 20:58:55 +0530

    fixed radio group onChange issue

--- micro-habit-studio/app/new-habit/page.tsx
+++ micro-habit-studio/app/new-habit/page.tsx
@@ -118,6 +118,7 @@ export default function NewHabitPage() {
               render={({ field, fieldState }) => (
                 <RadioGroup
                   {...field}
+                  onValueChange={field.onChange}
                   aria-invalid={fieldState.invalid}
                   className="flex"
                 >
